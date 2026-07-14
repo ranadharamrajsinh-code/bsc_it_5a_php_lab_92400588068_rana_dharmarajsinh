ff24aa3a27127c136c28dd4cf300fc50284df235
ranadharamrajsinh-code <ranadharamrajsinh@gmail.com>
2026-07-14 08:43:25 +0530

    Add files via upload

---
+++ prog4.1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $servername = "localhost:3307";
+        $username = "root";
+        $password = "";
+        $database = "studentdb";
+        $conn = new mysqli($servername, $username, $password, $database);
+
+        //$conn = new mysqli("localhost", "root", "", "studentdb");
+
+        /*if ($conn){
+        
+            echo "Database COnnected Successfully.";
+            }
+            else{
+                die("Connection Failed" . $conn->connect_error());
+            }*/
+
+             if (!$conn){
+                die("Connection failed:" . mysqli_connect_error());
+            }
+            else{
+                echo "Database Connected Successfully.";
+            }
+
+        $conn ->close();
+    ?>
+</body>
+</html>
